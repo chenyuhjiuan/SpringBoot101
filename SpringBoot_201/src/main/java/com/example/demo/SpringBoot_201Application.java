6f6b2bdf855f9cd831598b2a302c9217285eabc8
chenyuhjiuan <emilymarinovich@hotmail.com>
2019-05-20 15:27:39 -0400

    message changed

---
+++ SpringBoot_201/src/main/java/com/example/demo/SpringBoot_201Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot_201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot_201Application.class, args);
+	}
+
+}
